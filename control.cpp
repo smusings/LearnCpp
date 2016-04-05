db0eb32220172ffbd816ccba9ae708a22f4583ec
smusings <etyulmenkov@yahoo.com>
2016-04-05 19:42:38 -0400

    Getting more familair with loops. Cool to see we can do multiple inputs

--- control.cpp
+++ control.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main () {
@@ -28,5 +29,22 @@ int main () {
 		--n;
 	}
 
+	string strz;
+	do {
+		cout << "Enter text: ";
+		getline (cin, strz);
+		cout << "You entered: " << strz << "\n";
+	} while (strz != "goodbye");
+	
 	cout << "\n";
+
+	for (int n = 10; n>0; n--) {
+		cout << n << ", ";
+	}
+	cout << "\n";
+
+	for ( n = 100, int i = 100; n!=i; ++n, --n)
+	{
+		cout << n << ","<<i endl;
+	}
 }
