8fa34144d2ebe7bf8911124703040475d5083131
smusings <etyulmenkov@yahoo.com>
2016-04-08 19:44:57 -0400

    pass by ref vs pass by value

--- functions.cpp
+++ functions.cpp
@@ -17,6 +17,13 @@ int division(int a, int b) {
 	return a/b;
 }
 
+void duplicate(int& a, int& b, int& c)
+{
+	a*=2;
+	b*=2;
+	c*=2;
+}
+
 int main() {
 	int w = addition(5,4);
 	int x = subtraction(w,3);
@@ -26,4 +33,8 @@ int main() {
 	cout << "The Subtraction is " << x <<endl;
 	cout << "The Multiplication is " << y <<endl;
 	cout << "The Division is " << z <<endl;
+	duplicate(x,y,z);
+	cout << "x = " << x << ", y = " << y << ", z=" << z << endl;
+
+	return 0;
 }
