9dd33fe72f0bacba3c2cef33795ef2caf61d66b6
smusings <etyulmenkov@yahoo.com>
2016-04-12 19:28:19 -0400

    Print array functions

--- arrays.cpp
+++ arrays.cpp
@@ -2,14 +2,30 @@
 using namespace std;
 
 
+void print_array(int arg[], int length)
+{
+	for (int n=0; n<length; ++n)
+		cout << arg[n] << ' ';
+	cout << endl;
+}
+
+void print_two_d_array(int arg[][5], int x, int y)
+{
+	for (int i=0; i<x; ++i)
+		for(int j=0; j<y; ++j)
+			cout<< arg[i][j] << ' ';
+		cout << endl;
+	cout << endl;
+}
+
 int main()
 {
 	int foo[5] = {1, 2, 4, 5, 5};
-	cout << foo << endl;
+	print_array(foo, 5);
 	int bar[5] = {3, 2, 1};
-	cout << bar << endl;
+	print_array(bar, 5);
 	int baz[5] = {};
-	cout << baz << endl;
+	print_array(baz, 5);
 
 	int matrix[3][5];
 
@@ -21,8 +37,7 @@ int main()
 	{
 		matrix[n][0] = bar[n];
 	}
-
-	cout << matrix <<endl;
+	print_two_d_array(matrix, 3, 5);
 
 
 
