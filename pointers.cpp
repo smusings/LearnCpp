3a65079d4cd06bccb853417f548dc77519f659c8
smusings <etyulmenkov@yahoo.com>
2016-04-15 19:12:21 -0400

    Further experementing with pointers

--- pointers.cpp
+++ pointers.cpp
@@ -24,5 +24,41 @@ int main()
 	cout << "firstvalue is " << firstvalue <<endl;
 	cout << "secondvalue is " << secondvalue <<endl;
 
+
+	firstvalue = 5;
+	secondvalue = 15;
+	int * p1, * p2;
+
+	p1 = &firstvalue;	// p1 = address of firstvalue
+	p2 = &secondvalue;	//p2 = address of second value
+
+	*p1 = 10;		// value pointed to by p1=10
+	*p2 = *p1;		//value pointed by p2 = valye pointed to by p1
+	p1 = p2;		// p1 = p2 (valyue of pointer is copied)
+	*p1 = 20;		// valye pointed to by p1=20
+
+	cout << "firstvalue is " << firstvalue <<endl;
+	cout << "secondvalue is " << secondvalue <<endl;
+
+	int myarray [5];
+	int * arraypointer;
+
+	arraypointer = myarray;
+
+	* arraypointer = 10;
+	arraypointer++;
+	* arraypointer = 20;
+	arraypointer = &myarray[2];
+	* arraypointer = 30;
+	arraypointer = myarray+3;
+	* arraypointer = 40;
+	arraypointer = myarray;
+	* (arraypointer+4) = 50;
+
+	for (int n = 0; n <5; n++)
+	{
+		cout << myarray[n] << ", ";
+	}
+
 	return 0;
 }
