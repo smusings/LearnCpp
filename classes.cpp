67d3bfcaa5752a5aedbdc4b4a5313d41a95265bd
smusings <etyulmenkov@yahoo.com>
2016-04-20 20:46:36 -0400

    Classes

---
+++ classes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+	int width, height;
+public:
+	void set_values (int, int);
+	int area()
+	{
+		return width*height;
+	}
+};
+
+void Rectangle::set_values(int x, int y) {
+	width = x;
+	height = y;
+}
+
+int main() {
+	rectangle rect;
+	rect.set_values(3,4)
+	cout << "Area: " << rect.area();
+	return 0;
+}
