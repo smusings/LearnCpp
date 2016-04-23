bd321cabee8842c32775481a3b9929ffcb140960
smusings <etyulmenkov@yahoo.com>
2016-04-23 18:47:45 -0400

    More classes

--- classes.cpp
+++ classes.cpp
@@ -9,13 +9,21 @@ public:
 	int area (void) {return width*height;}
 };
 
-void Rectangle::Rectangle() {
-	width = 5
+class Circle {
+	double radius;
+public:
+	Circle();
+	Circle(double r) { radius = r;}
+	double circum() {return 2*radius*3.14159265;}
+};
+
+Rectangle::Rectangle() {
+	width = 5;
 	height = 5;
 }
 
-void Rectangle::Rectangle(int a, int b) {
-	width = a
+Rectangle::Rectangle(int a, int b) {
+	width = a;
 	height = b;
 }
 
@@ -24,5 +32,13 @@ int main() {
 	Rectangle rectb;
 	cout << "Area: " << rect.area() << endl;
 	cout << "Area: " << rectb.area() << endl;
+
+	Circle foo (10.0);
+	Circle bar = 20.0;
+
+	cout << "foo's circum: "<< foo.circum() << endl;
+	cout << "bar's circum: "<< bar.circum() << endl;
+
+
 	return 0;
 }
