c60cf29f4892be20c907188f50ffc1b52bdc2408
smusings <etyulmenkov@yahoo.com>
2016-05-06 21:37:24 -0400

    Friend classes

--- friend.cpp
+++ friend.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
 using namespace std;
 
+class Square;
+
 class Rectangle {
   int width, height;
 public:
-  Rectangle() {}
-  Rectangle (int x, int y) : width(x), height(y) {}
-  int area() {return width * height;}
-  friend Rectangle duplicate (const Rectangle&);
+    int area ()
+      {return (width * height);}
+    void convert (Square a);
 };
 
-Rectangle duplicate (const Rectangle& param)
-{
-  Rectangle res;
-  res.width = param.width*2;
-  res.height = param.height*2;
-  return res;
+class Square {  // rectangle is allowed access to everything
+  friend class Rectangle;
+private:
+  int side;
+public:
+  Square (int a) : side(a){}
+};
+
+void Rectangle::convert (Square a) {
+  width = a.side;
+  height = a.side;
 }
 
 int main () {
-  Rectangle foo;
-  Rectangle bar (2,3);
-  foo = duplicate (bar);
-  cout << foo.area() << '\n';
+  Rectangle rect;
+  Square sqr (4);
+  rect.convert(sqr);
+  cout << rect.area();
   return 0;
 }
